1f80fce85f02c5082e0cad81dd02fff9b534835d
Sri Abhishek Mamidi <66781982+sam5747@users.noreply.github.com>
2024-05-20 13:44:26 +0000

    changes for the assignment

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,61 @@
 import React, { Component } from "react";
-import "./Carousel.css";
+import "../components/Carousel.css";
 import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// implement the class below
 class Carousel extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = { currentImageIndex: 0 };
+    this.handleNext = this.handleNext.bind(this);
+    this.handlePrev = this.handlePrev.bind(this);
+  }
+
+  handleNext() {
+    this.setState((prevState) => ({
+      currentImageIndex:
+        prevState.currentImageIndex === images.length - 1
+          ? 0
+          : prevState.currentImageIndex + 1,
+    }));
+  }
+
+  handlePrev() {
+    this.setState((prevState) => ({
+      currentImageIndex:
+        prevState.currentImageIndex === 0
+          ? images.length - 1
+          : prevState.currentImageIndex - 1,
+    }));
+  }
+
+  render() {
+    const { currentImageIndex } = this.state;
+    return (
+      <div className="carousel-container">
+        <button className="prev-btn" onClick={this.handlePrev}>
+          <ArrowBackIosIcon />
+        </button>
+        <div className="image-container">
+          <img
+            className="carousel-image"
+            src={images[currentImageIndex].img}
+            alt={images[currentImageIndex].title}
+          />
+          <div className="image-text title">
+            <h2>{images[currentImageIndex].title}</h2>
+          </div>
+          <div className="image-text subtitle">
+            <p>{images[currentImageIndex].subtitle}</p>
+          </div>
+        </div>
+        <button className="next-btn" onClick={this.handleNext}>
+          <ArrowForwardIosIcon />
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Carousel;
